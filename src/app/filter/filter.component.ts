d0a54a88da614a7734dc905d36d764ce388564d7
kaisainen <kaisainen@gmail.com>
2022-03-29 16:27:44 +0300

    Add filter bar and enable filter tags to show/hide

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -2,11 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'filter',
-  templateUrl: './filter.component.html',
+  template: `
+    <div class="grid-container">
+      <div>PLACES</div>
+      <div (click)="showFilterTags()">FILTERS</div>
+    </div>
+    <div *ngIf="showTags" class="tag-container">
+      <p *ngFor="let tag of tags">
+        <span class="tag">#{{ tag }}</span>
+      </p>
+    </div>
+  `,
   styleUrls: ['./filter.component.css'],
 })
 export class FilterComponent implements OnInit {
+  tags = [
+    'cafe',
+    'gym',
+    'museum',
+    'zoo',
+    'restaurant',
+    'park',
+    'playground',
+    'shop',
+    'bakery',
+  ];
+  showTags = false;
+
   constructor() {}
+
+  showFilterTags() {
+    this.showTags = !this.showTags;
+  }
 
   ngOnInit(): void {}
 }
