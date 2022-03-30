3d254284597135cfcacba189f65189f37c610416
kaisainen <kaisainen@gmail.com>
2022-03-30 13:44:53 +0300

    Enable selecting filter tags

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -5,8 +5,8 @@ import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
   selector: 'filter',
   template: `
     <div class="grid-container">
-      <button class="filter-title">PLACES</button>
-      <button class="filter-title" (click)="showFilterTags()">
+      <button type="button" class="filter-title">PLACES</button>
+      <button type="button" class="filter-title" (click)="showFilterTags()">
         FILTERS
         <fa-icon *ngIf="!showTags" [icon]="faCaretRight"></fa-icon>
         <fa-icon *ngIf="showTags" [icon]="faCaretDown"></fa-icon>
@@ -14,7 +14,12 @@ import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons';
     </div>
     <div *ngIf="showTags" class="tag-container">
       <p *ngFor="let tag of tags">
-        <span class="tag">#{{ tag }}</span>
+        <span
+          (click)="addSelectedTag(tag)"
+          [ngClass]="getYellowClass(tag)"
+          class="tag"
+          >#{{ tag }}</span
+        >
       </p>
     </div>
   `,
@@ -35,11 +40,34 @@ export class FilterComponent implements OnInit {
   showTags = false;
   faCaretDown = faCaretDown;
   faCaretRight = faCaretRight;
+  selectedTags: string[] = [];
+  selectedTag = false;
 
   constructor() {}
 
   showFilterTags() {
     this.showTags = !this.showTags;
+  }
+
+  // ADDS AND REMOVES A CLICKED FILTER TAG TO ARRAY TO LATER BE USED FOR FILTERING PLACES/ACTIVITIES/EVENTS
+  addSelectedTag(tag: string): void {
+    const thisTag = tag;
+    if (!this.selectedTags.includes(thisTag)) {
+      this.selectedTags.push(thisTag);
+    } else if (this.selectedTags.includes(thisTag)) {
+      const index = this.selectedTags.indexOf(thisTag);
+      if (index > -1) {
+        this.selectedTags.splice(index, 1);
+      }
+    }
+  }
+
+  // ADDS AND REMOVES YELLOW BACKGROUND TO SELECTED FILTER TAGS
+  getYellowClass(tag: string) {
+    if (this.selectedTags.includes(tag)) {
+      return 'selectedtag';
+    }
+    return '';
   }
 
   ngOnInit(): void {}
