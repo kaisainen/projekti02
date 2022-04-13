ec943d7db6d2385cb27eb00d7025a340d79f9884
kaisainen <kaisainen@gmail.com>
2022-04-13 15:51:53 +0300

    Added functionalities to initialize maps

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as Leaflet from 'leaflet';
 
 @Injectable({
   providedIn: 'root',
@@ -45,5 +46,69 @@ export class MapService {
         maximumAge: 0,
       }
     );
+  }
+
+  // This function draws the map based on user location and adds a marker of the user's location. Takes a leaflet map object and a zoom amount as parameters
+  initMapByUserLocation(map: Leaflet.Map, zoom: number) {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    // gets current position
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      const lat = coords.latitude;
+      const long = coords.longitude;
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+      // sets the map view based on current location
+      if (!zoom) {
+        zoom = 15;
+      }
+      map = Leaflet.map('map').setView([lat, long], zoom);
+      // adds the tiles (pictures of map) to the map
+      const tiles = Leaflet.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+          attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          maxZoom: 18,
+          minZoom: 3,
+        }
+      );
+      tiles.addTo(map);
+
+      //adds a marker of current location on the map
+      let marker = Leaflet.marker([lat, long]).addTo(map);
+
+      marker.bindPopup('<b>My location</b>').openPopup();
+    });
+  }
+
+  // Draws a map with center of the map based on coordinates and zoom level given as parameters.
+  initMapByCoordinates(
+    map: Leaflet.Map,
+    lat: number,
+    long: number,
+    zoom: number
+  ) {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    if (!zoom) {
+      zoom = 15;
+    }
+    map = Leaflet.map('map').setView([lat, long], zoom);
+    // adds the tiles (pictures of map) to the map
+    const tiles = Leaflet.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        maxZoom: 18,
+        minZoom: 3,
+      }
+    );
+    tiles.addTo(map);
   }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-declare const L: any;
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as Leaflet from 'leaflet';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-map',
@@ -7,72 +8,12 @@ declare const L: any;
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
-  ngOnInit() {
-    this.getCurrentLocation();
-    this.watchPosition();
-  }
+  private map: any;
 
-  getCurrentLocation() {
-    if (!navigator.geolocation) {
-      console.log('location is not supported');
-    }
-    // gets current position
-    navigator.geolocation.getCurrentPosition((position) => {
-      const coords = position.coords;
-      const latLong = [coords.latitude, coords.longitude];
-      console.log(
-        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-      );
-      // sets the map view based on current location
-      let mymap = L.map('map').setView(latLong, 11);
-      // adds the tiles to the map
-      L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWgtMjAiLCJhIjoiY2wxbTg0dWZyMGdlaTNqb2JhbXVqaG90aiJ9.P2rhaDNS3sVsqmeewBeQpQ',
-        {
-          attribution:
-            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: 'your.mapbox.access.token',
-        }
-      ).addTo(mymap);
+  constructor(private mapService: MapService) {}
 
-      //adds a marker of current location on the map
-      let marker = L.marker(latLong).addTo(mymap);
-
-      marker.bindPopup('<b>My location</b>').openPopup();
-
-      //adds another marker on map for given coordinates - we need a separate function for this that takes coordinates as parameters so that we can add all locations from the Helsinki API and add their markers to the map
-      L.marker([60.1967887878418, 24.967309951782227])
-        .addTo(mymap)
-        .bindPopup('Added place.')
-        .openPopup();
-    });
-  }
-
-  watchPosition() {
-    let desLat = 0;
-    let desLon = 0;
-    let id = navigator.geolocation.watchPosition(
-      (position) => {
-        console.log(
-          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
-        );
-        //to not overload device cache
-        if (position.coords.latitude === desLat) {
-          navigator.geolocation.clearWatch(id);
-        }
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-      }
-    );
+  ngOnInit(): void {
+    this.mapService.initMapByUserLocation(this.map, 15);
+    this.mapService.watchPosition();
   }
 }
