2b2b920c463dd35eebb94f1501694a00191200f6
kaisainen <kaisainen@gmail.com>
2022-04-19 14:20:57 +0300

    moved list-map display to app-component
    required changes to list, map and app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { faList } from '@fortawesome/free-solid-svg-icons';
+import { faMap } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   title = 'projekti02';
+  shownMap = false;
+  shownList = true;
+  showTop = true;
+  screenHeight: any;
+  screenWidth: any;
+  desktop = false;
+  faList = faList;
+  faMap = faMap;
 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {
+    this.getScreenSize();
+  }
 
- 
-
-  
+  showMap() {
+    this.shownMap = true;
+    this.shownList = false;
+  }
+  showList() {
+    this.shownList = true;
+    this.shownMap = false;
+  }
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth > 429) {
+      this.desktop = true;
+      this.shownMap = false;
+      this.shownList = true;
+      this.showTop = false;
+    } else {
+      this.desktop = false;
+    }
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,9 +3,11 @@ import { faList } from '@fortawesome/free-solid-svg-icons';
 import { faMap } from '@fortawesome/free-solid-svg-icons';
 import { HostListener } from '@angular/core';
 import { MapService } from '../map.service';
-import { Places } from "./places";
-import {JsonService} from '../json.service';
+import { Places } from './places';
+import { JsonService } from '../json.service';
 
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -13,17 +15,9 @@ import {JsonService} from '../json.service';
 })
 export class ListComponent implements OnInit {
   places: Places[] = [];
-  screenHeight: any;
-  screenWidth: any;
-  desktop = false;
-  shownMap = false;
-  shownList = true; 
-  faList = faList;
-  faMap = faMap;
   mapService = new MapService();
-  
-  constructor(private jsonService: JsonService) {
-  }
+
+  constructor(private jsonService: JsonService) {}
   getPlaces(): void {
     this.jsonService.getPlaces().subscribe((res: Places) => {
       this.places.push(res);
@@ -32,32 +26,8 @@ export class ListComponent implements OnInit {
   getPosition() {
     this.mapService.getMyLocation();
   }
-  showMap() {
-    this.shownMap = true;
-    this.shownList = false;
-  }
-  showList() {
-    this.shownList = true;
-    this.shownMap = false;
-  }
-  @HostListener('window:resize', ['$event'])
-  getScreenSize() {
-    this.screenHeight = window.innerHeight;
-    this.screenWidth = window.innerWidth;
-    if (this.screenWidth > 600) {
-      this.desktop = true;
-      this.shownMap = false;
-      this.shownList = true;
-    } else {
-      this.desktop = false;
-    }
-  }
   ngOnInit(): void {
     this.getPlaces();
-    this.getScreenSize();
     // this.list.sort((a, b) => (a.distance > b.distance) ? 1 : -1)
   }
-
-  
-
 }
