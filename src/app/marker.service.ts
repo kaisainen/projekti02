9c207b6ea1e6e98e2e034cb1ac62baf303b03abd
ahlem-17 <ahlem.mh2020@icloud.com>
2022-04-19 11:07:07 +0300

    working from places.json

--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as L from 'leaflet';
+import {  Datum, Places } from './list/places';
 import { PopupService } from './popup.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarkerService {
-  places: string = './assets/test.json';
-
+ // places: string = './assets/test.json';
+ //places: Places[] = [];
+ place: string = '../../assets/places.json';
+ 
   constructor(private http: HttpClient, private popupService: PopupService) {}
 
   makePlacesMarkers(map: L.Map): void {
-    this.http.get(this.places).subscribe((res: any) => {
-      for (const c of res) {
+    this.http.get(this.place).subscribe((res: any) => {
+      for (const c of res.data) {
         const lon = c.location.lon;
         const lat = c.location.lat;
         const marker = L.marker([lat, lon]);
