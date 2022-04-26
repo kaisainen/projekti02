7773fe163eedb01cc7aba0819f2db5f9b8061879
kaisainen <kaisainen@gmail.com>
2022-04-26 13:48:01 +0300

    add distance property

--- src/app/list/places.ts
+++ src/app/list/places.ts
@@ -5,83 +5,84 @@ export interface Places {
 }
 
 export interface Datum {
-  id:                string;
-  name:              NameClass;
-  source_type:       EType;
-  info_url:          string;
-  modified_at:       Date;
-  location:          Location;
-  description:       Description;
-  tags:              Tag[];
+  id: string;
+  name: NameClass;
+  source_type: EType;
+  info_url: string;
+  modified_at: Date;
+  location: Location;
+  description: Description;
+  tags: Tag[];
   extra_searchwords: string[];
   opening_hours_url: string;
+  distance: number;
 }
 
 export interface Description {
-  intro:  string;
-  body:   string;
+  intro: string;
+  body: string;
   images: Image[];
 }
 
 export interface Image {
-  url:              string;
+  url: string;
   copyright_holder: string;
-  license_type:     EType;
-  media_id:         string;
+  license_type: EType;
+  media_id: string;
 }
 
 export interface EType {
-  id:   number;
+  id: number;
   name: NameEnum;
 }
 
 export enum NameEnum {
-  Copyright1 = "Copyright 1",
-  Copyright2 = "Copyright 2",
-  Matko = "Matko",
+  Copyright1 = 'Copyright 1',
+  Copyright2 = 'Copyright 2',
+  Matko = 'Matko',
 }
 
 export interface Location {
-  lat:     number;
-  lon:     number;
+  lat: number;
+  lon: number;
   address: Address;
 }
 
 export interface Address {
   street_address: string;
-  postal_code:    string;
-  locality:       Locality;
-  neighbourhood:  string;
+  postal_code: string;
+  locality: Locality;
+  neighbourhood: string;
 }
 
 export enum Locality {
-  Empty = "",
-  Espoo = "Espoo",
-  Harviala = "Harviala,",
-  Helsingfors = "Helsingfors",
-  Helsingin = "Helsingin",
-  Helsinki = "Helsinki",
-  Hyvinkää = "Hyvinkää",
-  Hämeenlinna = "Hämeenlinna",
-  Inkoo = "Inkoo",
-  Järvenpää = "Järvenpää",
-  Kirkkonummi = "Kirkkonummi",
-  Lapinjärvi = "Lapinjärvi",
-  LocalityHELSINKI = "HELSINKI",
-  LocalityVantaa = "vantaa",
-  Luoma = "Luoma,",
-  Masala = "Masala",
-  Ojakkala = "Ojakkala",
-  Otalampi = "Otalampi",
-  Palojoki = "Palojoki",
-  Porvoo = "Porvoo",
-  Sipoo = "Sipoo",
-  Söderkulla = "Söderkulla",
-  Talma = "Talma",
-  Tervalampi = "Tervalampi",
-  Tuusula = "Tuusula",
-  Vanda = "Vanda",
-  Vantaa = "Vantaa",
+  Empty = '',
+  Espoo = 'Espoo',
+  Harviala = 'Harviala,',
+  Helsingfors = 'Helsingfors',
+  Helsingin = 'Helsingin',
+  Helsinki = 'Helsinki',
+  Hyvinkää = 'Hyvinkää',
+  Hämeenlinna = 'Hämeenlinna',
+  Inkoo = 'Inkoo',
+  Järvenpää = 'Järvenpää',
+  Kirkkonummi = 'Kirkkonummi',
+  Lapinjärvi = 'Lapinjärvi',
+  LocalityHELSINKI = 'HELSINKI',
+  LocalityVantaa = 'vantaa',
+  Luoma = 'Luoma,',
+  Masala = 'Masala',
+  Ojakkala = 'Ojakkala',
+  Otalampi = 'Otalampi',
+  Palojoki = 'Palojoki',
+  Porvoo = 'Porvoo',
+  Sipoo = 'Sipoo',
+  Söderkulla = 'Söderkulla',
+  Talma = 'Talma',
+  Tervalampi = 'Tervalampi',
+  Tuusula = 'Tuusula',
+  Vanda = 'Vanda',
+  Vantaa = 'Vantaa',
 }
 
 export interface NameClass {
@@ -92,7 +93,7 @@ export interface NameClass {
 }
 
 export interface Tag {
-  id:   string;
+  id: string;
   name: string;
 }
 
