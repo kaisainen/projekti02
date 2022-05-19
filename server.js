08cfaa630d252a9c8f4f40fd66fbb92a2c70fd1a
s2100628-Alex <alex.degerman.dev@gmail.com>
2022-05-19 22:11:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/<projekti02>"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<projekti02>/index.html"));
+app.use(express.static(__dirname + "/dist/projekti02"));
+app.get('*', (req, res) => {
+  res.sendFile(`/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 app.listen(process.env.PORT || 8080);
